a382ae2b3821af13e627edeb7d0d940dba793b63
Koyonari <yongshyan.an@gmail.com>
2024-05-22 12:35:33 +0800

    Currency converter layout

---
+++ app/src/main/java/sg/edu/np/mad/travelhub/ConvertCurrency.java
@@ -0,0 +1,39 @@
+package sg.edu.np.mad.travelhub;
+
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class ConvertCurrency extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_convert_currency);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Initialize the adapter
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.currencies, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Set the adapter for the first spinner
+        Spinner startSpinner = findViewById(R.id.start);
+        startSpinner.setAdapter(adapter);
+
+        // Set the adapter for the second spinner
+        Spinner endSpinner = findViewById(R.id.end);
+        endSpinner.setAdapter(adapter);
+    }
+}
